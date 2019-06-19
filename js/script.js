6663afb3d42aa166a887d07d6e51407a62bbf535
Wuqian Ye <natsumi3613@qq.com>
2019-06-19 22:10:23 +0000

    moving

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//c9.io moving
 // Pig Latin takes the first consonant (or consonant cluster) of an English 
 // word, moves it to the end of the word and suffixes an ay, or if a word
 // begins with a vowel you just add ay to the end. For example, pig becomes
